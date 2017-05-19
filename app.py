2a161652ce788433bbdfa9dbaf236f4f09ac22ef
Nara Huang <narahuang@gmail.com>
2017-05-20 00:32:12 +0800

    Add function switch:gf

--- app.py
+++ app.py
@@ -41,14 +41,24 @@ def callback():
 
 # ================= 機器人區塊 Start =================
 @handler.add(MessageEvent, message=TextMessage)  # default
+
+#def gf(query)
+#    gf_company_name = get_company_name(query)
+#    gf_company_summary = get_company_summary(query)
+#    return 
+
 def handle_text_message(event):                  # default
     msg = event.message.text #message from user
-
+    commandlist = msg.split()
+    jobname = commandlist[0]
+    {
+            gf : lambda: result = get_company_name(commandlist[1]) + get_company_summary(commandlist[1])
+            }.get(jobname, lambda: result = "gf 後面接股票代碼可以查詢 Google Finance 上面的公司資料唷！")
 
     # Google Finance 查詢公司名稱簡介
-    gf_company_name = get_company_name(msg)
-    gf_company_summary = get_company_summary(msg)
-    result = gf_company_name + gf_company_summary
+#    gf_company_name = get_company_name(msg)
+#    gf_company_summary = get_company_summary(msg)
+#    result = gf_company_name + gf_company_summary
 
     # 針對使用者各種訊息的回覆 Start =========
     line_bot_api.reply_message(
